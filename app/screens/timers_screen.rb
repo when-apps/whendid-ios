ed7dff257f628a1ba3a2f47691506520dce65161
Jonathan Hoyt <hoyt@github.com>
2014-03-22 21:49:04 -0700

    Show easy to read distance in time
    Closes #6

--- app/screens/timers_screen.rb
+++ app/screens/timers_screen.rb
@@ -12,7 +12,7 @@ class TimersScreen < PM::TableScreen
       :cells => Timer.sort_by(:happened_at, order: :descending).map do |timer|
         {
           :title     => timer.name,
-          :subtitle  => "#{(Time.now - timer.happened_at).to_i} seconds ago",
+          :subtitle  => "#{humanize_time_in_seconds(Time.now - timer.happened_at)} ago",
           :action    => :edit_timer,
           :arguments => {:timer => timer}
         }
@@ -39,5 +39,26 @@ class TimersScreen < PM::TableScreen
       userInfo: nil,
       repeats:  true
     )
+  end
+
+  def humanize_time_in_seconds(seconds)
+    values = [
+      [60, :seconds],
+      [60, :minutes],
+      [24, :hours],
+      [365, :days],
+      [100, :years]
+    ].map do |count, name|
+      if seconds > 0
+        seconds, n = seconds.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    end.compact.reverse
+
+    if values.size > 1
+      values[0..-2]
+    else
+      values
+    end.join(" ")
   end
 end
