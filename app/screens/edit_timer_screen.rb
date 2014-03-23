668c1530adb762cbcf375ba608b9529e904b23bf
Jonathan Hoyt <hoyt@github.com>
2014-03-22 20:02:28 -0700

    Add delete timer screen with confirmation step

--- app/screens/edit_timer_screen.rb
+++ app/screens/edit_timer_screen.rb
@@ -3,6 +3,16 @@ class EditTimerScreen < PM::FormotionScreen
 
   attr_accessor :timer
 
+  def on_create(args={})
+    super(args)
+
+    set_nav_bar_button :right, title: "Delete", action: :delete_timer
+  end
+
+  def delete_timer
+    open DeleteTimerScreen.new(:timer => timer)
+  end
+
   def on_submit(_form)
     formValues = _form.render
     timer.name = formValues[:name]
